3ce43a4a46b0b49233b7cf808defa35cbb65ed56
Soner Demirci <116302869+Soner08@users.noreply.github.com>
2022-11-26 19:15:06 +0300

    dovusOyunu

--- Main.java
+++ Main.java
@@ -1,23 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Teacher t1= new Teacher("Lale Şiviloğlu","5436","TRH" );
-        Teacher t2= new Teacher("Süleyman SEVİMLİ","2345","ÇRFY");
-        Teacher t3= new Teacher("Metin Lokumcu","2345","MAT");
+        Fighter marc = new Fighter("Eray" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Mert" , 10 , 95, 100, 0);
 
-
-        Course tarih=new Course("Tarih","TRH101","TRH");
-        tarih.addTeacher(t1);
-
-        Course coğrafya=new Course("Çoğrafya","ÇRFY102","ÇRFY");
-        coğrafya.addTeacher(t2);
-
-        Course matematik=new Course("Matematik","MAT103","MAT");
-        matematik.addTeacher(t3);
-
-        Student s1=new Student("Soner DEMİRCİ","6043","12/B",tarih, matematik,coğrafya);
-        s1.addBulkExamNote(45,68,99);
-        s1.isPass();
-
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
 
     }
 }
